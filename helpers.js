5b4a0e1222c01634def8c5c3f1b1c2d556ceaad1
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2025-02-04 23:16:26 +0100

    Complete code

---
+++ helpers.js
@@ -0,0 +1,32 @@
+export const isArmstrong = (number) => {
+    const sum = number.split('').reduce((acc, num) => acc + Math.pow(parseInt(num), number.length), 0)
+    
+    return sum == number
+}
+
+export const isPrimeNumber = (number) => {
+    if (number < 2) return false
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) return false
+    }
+    return true
+}
+
+export const isPerfectNumber = (number) => {
+    if (number < 2) {
+        return false
+    }
+
+    let sum = 1
+
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            sum += i
+            if (i !== number / i) {
+                sum += number / i
+            }
+        }
+    }
+
+    return sum === number
+}
---
+++ index.js
@@ -0,0 +1,66 @@
+import express from 'express'
+import cors from 'cors'
+import fetch from 'node-fetch'
+
+import { isArmstrong, isPrimeNumber } from './helpers.js'
+
+const app = express()
+
+const PORT = 3000
+
+app.use(cors())
+
+app.get('/api/classify-number', async (req, res) => {
+    const { number } = req.query
+
+    const isInteger = Number.isInteger(parseInt(number))
+
+    if (!isInteger) {
+        return res.status(400)
+            .json({
+                number,
+                error: true
+            })
+    }
+
+    const NUMBERS_API_URL = `http://numbersapi.com/${number}/math`
+
+    const funFact = await fetch(NUMBERS_API_URL)
+    const data = await funFact.text()
+
+    const properties = []
+
+    const sumDigits = number.toString().split('').reduce((acc, num) => acc + parseInt(num), 0)
+
+    const isArmstrongNumber = isArmstrong(number) ? 'armstrong' : null
+    const checkIsOddOrEven = number % 2 !== 0 ? 'odd' : 'even'
+    const is_prime = isPrimeNumber(number)
+
+    if (isArmstrongNumber) {
+        properties.push(isArmstrongNumber, checkIsOddOrEven)
+    } else {
+        properties.push(checkIsOddOrEven);
+    }
+
+    return res.status(200)
+        .json({
+            "number": parseInt(number),
+            is_prime,
+            "is_perfect": false,
+            properties,
+            "digit_sum": sumDigits,
+            "fun_fact": data
+        })
+})
+
+app.use('*', (_req, res) => {
+    res.status(404)
+        .json({
+            error: true
+        })
+    return
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running on port 3000')
+})
